35f8fa56d906f08dd7cd37c9fe3c96692100e177
tea <40557944+cctv86@users.noreply.github.com>
2020-05-13 09:51:45 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+version = '1.0.0'
+
+setup(
+    name="redis-sync",
+    version=version,
+    description='Redis sync tool that public cloud',
+    url='https://github.com/cxjchocolate/alerta-contrib',
+    license='MIT',
+    author='zhigang.hong',
+    author_email='627849521@qq.com',
+    packages=find_packages(),
+    py_modules=['redis-sync'],
+    install_requires=['redis >= 3.5.1',],
+    include_package_data=True,
+    zip_safe=True,
+)
